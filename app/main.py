e0ab29bde89edfb9e82d2ae62a795fe54ac39c40
mascarene <quentin@outlook.sg>
2022-12-14 13:00:09 +0100

    feat: delete function will remove post from db

--- app/main.py
+++ app/main.py
@@ -107,13 +107,17 @@ def find_index_post(id):
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     # trouver l'index dans le tableau qui a le bon ID
-    index = find_index_post(id)
+    # index = find_index_post(id)
+    cursor.execute(""" DELETE * FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    deleted_post =  cursor.fetchone()
+    conn.commit()
 
-    if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Le post avec l'id:{id} n'existe pas")
+    if deleted_post is None:
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT,
+                            detail=f"Le post avec l'id:{id} n'existe pas")
 
-    my_posts.pop(index)
+    # my_posts.pop(index)
     # Does not work with HTTP 204:
     # return {"message": f"Le post {id} à bien été supprimé"}
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
 
