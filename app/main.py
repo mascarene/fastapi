5ce9858a5882d4581e35bcee415da8398ae4d055
mascarene <quentin@outlook.sg>
2022-12-16 15:29:30 +0100

    Update post in DB

--- app/main.py
+++ app/main.py
@@ -71,16 +71,23 @@ def find_post(id):
 
 @app.put("/post/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
 
-    if index == None:
+    cursor.execute(
+        """ UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
+                        (post.title, post.content, post.published, str(id)))
+
+    updated_post = cursor.fetchone()
+    conn.commit()
+    # index = find_index_post(id)
+
+    if update_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Le post avec l'id:{id} n'existe pas")
 
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {'data' : post_dict}
+    # post_dict = post.dict()
+    # post_dict['id'] = id
+    # my_posts[index] = post_dict
+    return {'data' : updated_post}
 
 @app.get("/posts/latest")
 def get_latest_post():
