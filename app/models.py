a2f57a7774d66f61f3c463a265b9f034b6efcbac
mascarene <quentin@outlook.sg>
2022-12-17 22:24:21 +0100

    fix title column not nullable

--- app/models.py
+++ app/models.py
@@ -5,6 +5,6 @@ class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key = True, nullable = False)
-    title = Column(String)
+    title = Column(String, nullable=False)
     content = Column(String, nullable = False)
     published = Column(Boolean, default = True)
