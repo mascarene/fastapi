aeef2d9cd4c02fd6237d67ccb194347de56614b7
mascarene <quentin@outlook.sg>
2022-12-17 22:32:05 +0100

    fix have a default value for published column

--- app/models.py
+++ app/models.py
@@ -5,6 +5,6 @@ class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key = True, nullable = False)
-    title = Column(String, nullable=False)
+    title = Column(String, nullable = False)
     content = Column(String, nullable = False)
-    published = Column(Boolean, default = True)
+    published = Column(Boolean, server_default = 'True', nullable = False)
