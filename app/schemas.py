34c6dba13b4c7fe6b82070c3b4fb205a32cd2550
mascarene <quentin@outlook.sg>
2022-12-22 18:49:58 +0100

    define full response

--- app/schemas.py
+++ app/schemas.py
@@ -9,8 +9,10 @@ class PostCreate(PostBase):
     pass
 
 class Post(BaseModel):
+    id: int
     title: str
     content: str
     published: bool
+    created_at: datetime
     class Config:
         orm_mode = True
