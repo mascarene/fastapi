d7ead79ccf9bc3b7fdbe2ac7f40d65dd354bf44e
mascarene <quentin@outlook.sg>
2022-12-22 18:51:29 +0100

    clean response model

--- app/schemas.py
+++ app/schemas.py
@@ -8,11 +8,8 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     pass
 
-class Post(BaseModel):
+class Post(PostBase):
     id: int
-    title: str
-    content: str
-    published: bool
     created_at: datetime
     class Config:
         orm_mode = True
