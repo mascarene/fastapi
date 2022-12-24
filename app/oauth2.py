6ae49ff330d8e280ee38c0a8348d8b31051dd5eb
mascarene <quentin@outlook.sg>
2022-12-24 15:46:12 +0100

    protecting routes

--- app/oauth2.py
+++ app/oauth2.py
@@ -22,22 +22,19 @@ def create_access_token(data: dict):
 
 def verify_access_token(token: str, credentials_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id: str = payload.get("user_id")
-
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
-
     except JWTError:
         raise credentials_exception
-
     return token_data
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED, 
-        detail="La vérification des identifiant à échouée.", 
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="La vérification des identifiant à échouée.",
         headers={"WWW-Authenticate": "Bearer"})
 
     return verify_access_token(token, credentials_exception)
--- app/routers/post.py
+++ app/routers/post.py
@@ -16,7 +16,8 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), get_current_user: int = Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+    print(user_id)
     new_post = models.Post(**post.dict())
 
     db.add(new_post)
