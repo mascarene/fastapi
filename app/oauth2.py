ebc8d4ba3a5a7fab404ae04c6851b497d6a892fd
mascarene <quentin@outlook.sg>
2022-12-24 10:02:36 +0100

    feat: verify user is logged in

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,11 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
 
 # openssl rand -hex 32
 SECRET_KEY = "72cb597c794195196863d8fd6d8746aaf6fe073b245f17be2e7b28626a168314"
@@ -8,8 +14,30 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, 
+        detail="La vérification des identifiant à échouée.", 
+        headers={"WWW-Authenticate": "Bearer"})
+
+    return verify_access_token(token, credentials_exception)
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import Optional, List
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from ..database import get_db
 
 router = APIRouter(
@@ -16,7 +16,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), get_current_user: int = Depends(oauth2.get_current_user)):
     new_post = models.Post(**post.dict())
 
     db.add(new_post)
