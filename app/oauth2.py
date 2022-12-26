b5e9a2db772a7928484ab8081c41541acf0c2305
mascarene <quentin@outlook.sg>
2022-12-26 10:23:06 +0100

    fix payload

--- app/oauth2.py
+++ app/oauth2.py
@@ -23,7 +23,7 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id: str = payload.get("user_id")
+        id: str = payload.get("userid")
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
@@ -34,7 +34,7 @@ def verify_access_token(token: str, credentials_exception):
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="La vérification des identifiant à échouée.",
+        detail="La vérification des identifiants à échouée.",
         headers={"WWW-Authenticate": "Bearer"})
 
     return verify_access_token(token, credentials_exception)
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from typing import Optional, List
+from typing import List, Optional
+
+from sqlalchemy import func
 from .. import models, schemas, oauth2
 from ..database import get_db
 
 router = APIRouter(
-    prefix = "/posts",
-    tags = ['Posts'] # Auto-doc categories
+    prefix="/posts",
+    tags=['Posts']
 )
 
 @router.get("/posts", response_model=List[schemas.Post])
@@ -16,8 +18,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
-    print(user_id)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     new_post = models.Post(**post.dict())
 
     db.add(new_post)
