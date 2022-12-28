fcc07a11351081b0006f45a0998864281ce52305
mascarene <quentin@outlook.sg>
2022-12-28 10:52:04 +0100

    only retrieving logged-in user's

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,8 +12,8 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     return posts
 
 
