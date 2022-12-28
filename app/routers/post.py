a31c18d3f0806280b54b9d01fc4cb7a763080909
mascarene <quentin@outlook.sg>
2022-12-28 15:15:03 +0100

    query parameters

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,8 +12,9 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0):
+
+    posts = db.query(models.Post).limit(limit).offset(skip).all()
     return posts
 
 
