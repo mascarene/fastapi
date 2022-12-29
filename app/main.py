42f10d436984e5540247755ba8efaeb97db5c280
mascarene <quentin@outlook.sg>
2022-12-29 10:46:45 +0100

    clean imports

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post,user, auth
-from .config import settings
 
 # Automatic documentation (Swagger UI):
 # http://127.0.0.1:8000/docs/
