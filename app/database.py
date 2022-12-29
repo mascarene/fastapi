b192c8fb6b650cef9923c511f1759a0ae2ced7b7
mascarene <quentin@outlook.sg>
2022-12-29 09:29:19 +0100

    clean

--- app/database.py
+++ app/database.py
@@ -6,22 +6,14 @@ import psycopg2
 import time
 from psycopg2.extras import RealDictCursor
 
-
-# Se connecter à la base de données
-# structure de l'URL pour Postgres : postgresql://<username>:<password>@<ip-address/hostname>/<database_name>
-# To-Do: Secrets !
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:2YuHHakD7fy6tK88aHm2jd7cuKZMFbjsEBYoYGZtMHtX8BPBsLedxEx3mxvANaNWM8zQdqvPjE7oXZzp37KwhRv7iAd3LNwEVjJj58A27GPHeBPvwyBjWfNQwNvygg79@localhost/fastapi'
-
-# Création du moteur (qui va connecter SQLAlchemy a la base Postgres)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Declarative Mapping: Définie la classe 'Base', tous les modèles que nous définirons pour creer nos tables dans Postgress utiliseront cette classe.
 Base = declarative_base()
 
-# Dépendance :
 def get_db():
     db = SessionLocal()
     try:
