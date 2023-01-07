8ff39302330cf33d22f6537134d0c0761806eed9
mascarene <quentin@outlook.sg>
2023-01-07 13:39:26 +0100

    clean import order

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import post,user, auth, vote
-from fastapi.middleware.cors import CORSMiddleware
+
 
 # Automatic documentation (Swagger UI):
 # http://127.0.0.1:8000/docs/
