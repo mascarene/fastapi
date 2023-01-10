d860f2d578cff5cc455eead651850dd976e0e782
mascarene <quentin@outlook.sg>
2023-01-10 13:34:35 +0100

    dummy tests

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,7 +1,6 @@
 import pytest
 from app.calc import *
 
-# Parametrization 
 @pytest.mark.parametrize("num1, num2, expected",
     [
         (3, 2, 5),
@@ -10,3 +9,27 @@ from app.calc import *
     ])
 def test_add(num1, num2, expected):
     assert add(num1, num2) == expected
+
+def test_bank_set_initial_amount():
+    bank_account = BankAccount(50)
+    assert bank_account.balance == 50
+
+def test_bank_default_amount():
+    bank_account = BankAccount()
+    assert bank_account.balance == 0
+
+def test_bank_withdraw():
+    bank_account = BankAccount(50)
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
+
+def test_bank_deposit():
+    bank_account = BankAccount(50)
+    bank_account.deposit(50)
+    assert bank_account.balance == 100
+
+def test_bank_interest():
+    bank_account = BankAccount(50)
+    bank_account.collect_interest()
+    # as float will fail, rounded it
+    assert round(bank_account.balance, 6) == 55
