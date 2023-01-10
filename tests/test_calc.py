1b072caf8b556b9f58536938a686e58b7ed6939e
mascarene <quentin@outlook.sg>
2023-01-10 13:53:06 +0100

    pytest combining fixtures and parametrization

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -10,33 +10,13 @@ def zero_bank_account():
 def bank_account():
     return BankAccount(50)
 
-
-# Parametrization
-@pytest.mark.parametrize("num1, num2, expected",
+@pytest.mark.parametrize("x, y, z",
     [
-        (3, 2, 5),
-        (2, 7, 9),
-        (1, 1, 2)
+        (200, 100, 100),
+        (50, 10, 40),
+        (1200, 200, 1000)
     ])
-def test_add(num1, num2, expected):
-    assert add(num1, num2) == expected
-
-# Dummy classes tests
-
-def test_bank_default_amount(zero_bank_account):
-    assert zero_bank_account.balance == 0
-
-def test_bank_withdraw(bank_account):
-    bank_account.withdraw(20)
-    assert bank_account.balance == 30
-
-def test_bank_deposit(bank_account):
-    bank_account.deposit(50)
-    assert bank_account.balance == 100
-
-def test_bank_interest(bank_account):
-    bank_account.collect_interest()
-    # as float will fail, rounded it
-    assert round(bank_account.balance, 6) == 55
-
- 
+def test_bank_transaction(zero_bank_account, x, y, z):
+    zero_bank_account.deposit(x)
+    zero_bank_account.withdraw(y)
+    assert zero_bank_account.balance == z
