5206f90b78da89300e707dfccff321f15ec1fd91
mascarene <quentin@outlook.sg>
2023-01-10 13:59:05 +0100

    testing exceptions

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -14,9 +14,14 @@ def bank_account():
     [
         (200, 100, 100),
         (50, 10, 40),
-        (1200, 200, 1000)
+        (1200, 200, 1000),
+        # (0, 100, 0)
     ])
 def test_bank_transaction(zero_bank_account, x, y, z):
     zero_bank_account.deposit(x)
     zero_bank_account.withdraw(y)
     assert zero_bank_account.balance == z
+
+def test_insufficient_founds(bank_account):
+    with pytest.raises(InsufficientFunds):
+        bank_account.withdraw(100)
