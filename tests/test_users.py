b1c035d88fa15341c44e245843db4639c635bb9e
mascarene <quentin@outlook.sg>
2023-01-11 16:56:08 +0100

    setup testing database

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,36 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.database import get_db
 from app.main import app
+from app import schemas
+from app.config import settings
+from app.database import Base
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Base = declarative_base()
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Swap database, will get a different session object:
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+Base.metadata.create_all(bind=engine)
+app.dependency_overrides[get_db] == override_get_db
 
 client = TestClient(app)
 
-def test_root():
-    res = client.get("/")
-    print(res.json().get('message'))
-    assert res.json().get('message') == 'Bienvenue sur mon API'
-    assert res.status_code == 200
+def test_create_user():
+    res = client.post("/users/", json={"email": "pizza@pizza.it", "password": "pepperoni"})
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == ""
+    assert res.status_code == 201
