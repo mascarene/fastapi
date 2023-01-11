0d72a9efc4483a1d572ac36cd8e9ca39761a5ae2
mascarene <quentin@outlook.sg>
2023-01-11 23:04:01 +0100

    fixtures to handle database interaction

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,42 +1,5 @@
-from fastapi.testclient import TestClient
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from alembic import command
-from app.database import get_db
-from app.main import app
 from app import schemas
-from app.config import settings
-from app.database import Base
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Swap database, will get a different session object:
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] == override_get_db
-
-
-@pytest.fixture
-def client():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    # command.upgrade("head")
-    # command.downgrade("base")
-    yield TestClient(app)
-
+from .database import client, session
 
 def test_create_user(client):
     res = client.post("/users/", json={"email": "pizza@pizza.it", "password": "pepperoni"})
