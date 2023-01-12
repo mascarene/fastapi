4697f4946141319e3785ddd87247a0ddf25912c3
mascarene <quentin@outlook.sg>
2023-01-12 17:34:05 +0100

    pytest fixture scope

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,9 @@
 from app import schemas
 from .database import client, session
 
-# def test_root(client):
-#     res = client.get("/")
-#     print(res.json().get('message'))
-#     assert res.status_code == 200
+def test_root(client):
+    res = client.get("/")
+    assert res.status_code == 200
 
 def test_create_user(client):
     res = client.post("/users/", json={"email": "pizza@pizza.it", "password": "pepperoni"})
@@ -12,7 +11,7 @@ def test_create_user(client):
     assert new_user.email == "pizza@pizza.it"
     assert res.status_code == 201
 
-def test_login(client):
+def test_login_user(client):
     res = client.post(
-        "/login", data={"email": "pizza@pizza.it", "password": "pepperoni"})    
+        "/login", data={"username": "pizza@pizza.it", "password": "pepperoni"})
     assert res.status_code == 200
