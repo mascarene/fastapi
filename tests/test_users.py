c4d446abe9f05ef6da5bfabeb315e940cedd0a63
mascarene <quentin@outlook.sg>
2023-01-12 13:15:07 +0100

    test user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,18 @@
 from app import schemas
 from .database import client, session
 
+# def test_root(client):
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.status_code == 200
+
 def test_create_user(client):
     res = client.post("/users/", json={"email": "pizza@pizza.it", "password": "pepperoni"})
     new_user = schemas.UserOut(**res.json())
-    assert new_user.email == ""
+    assert new_user.email == "pizza@pizza.it"
     assert res.status_code == 201
+
+def test_login(client):
+    res = client.post(
+        "/login", data={"email": "pizza@pizza.it", "password": "pepperoni"})    
+    assert res.status_code == 200
