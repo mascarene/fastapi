32a5dbeb1e17e67350e46e079f1c24d115c8f0e0
mascarene <quentin@outlook.sg>
2023-01-14 12:07:54 +0100

    test incorrect login

--- tests/test_users.py
+++ tests/test_users.py
@@ -23,3 +23,17 @@ def test_login_user(test_user, client):
 #     res = client.post("/login", data={"username": test_user['email'], "password": "tacos"})
 #     assert res.status_code == 403
 #     assert res.json().get('detail') == 'Identifiants invalides'
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrong@email.com', 'pepperoni', 403),
+    ('pizza@pizza.it', 'wrong', 403),
+    ('wrong@email.com', 'wrong', 403),
+    (None, 'pepperoni', 422),
+    ('pizza@pizza.it', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
